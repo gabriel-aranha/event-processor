db792626a3220c6da2645c3296da977e8f798648
Gabriel Aranha Martins <gabriel.aranha.martins@gmail.com>
2021-03-17 02:35:37 +0000

    Add cloud execution check

--- app.py
+++ app.py
@@ -13,8 +13,11 @@ def health_check():
 @app.route('/activate', methods=['GET'])
 def activate_processor():
     event_processor.init_event_consuming()
+    return {'status': 'ok'}, 200
 
 
 if __name__ == "__main__":
+    if os.getenv('CLOUD'):
+        time.sleep(60)
     event_processor.init_event_consuming()
     app.run(port=int(os.getenv('PORT')))

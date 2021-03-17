f8d64a29526a99f59ca17f9e90ef7e5f547558ca
Gabriel Aranha Martins <gabriel.aranha.martins@gmail.com>
2021-03-17 01:50:23 +0000

    Removed unused file

--- event_processor_test.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import event_processor
-import json_schema
-
-
-def test_validate_json_message_schema():
-    expected = {'status': 'ok'}
-    assert expected == json.loads(res.get_data(as_text=True))

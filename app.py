c445b87367de344e28b7f7fb98e32522e6b2fead
Gabriel Aranha Martins <gabriel.aranha.martins@gmail.com>
2021-03-17 02:08:53 +0000

    Changed Procfile type

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
+from flask import Flask
 import event_processor
+import time
+import os
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def health_check():
+    return {'status': 'ok'}, 200
 
 
 if __name__ == "__main__":
     event_processor.init_event_consuming()
+    app.run(port=int(os.getenv('PORT')))
